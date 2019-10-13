f9971c384ee7d1c934f55947bd12d905422f0c6c
Jared S <jaredjames2020@gmail.com>
2019-10-13 16:35:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,9 +1,9 @@
 # Your code goes here!
 class Anagram
-  attr_accessor :match
+  attr_accessor :word
   
   def initialize(word)
-    @word = %w(word)
+    @word = word
   end
   
   def match
@@ -12,6 +12,13 @@ class Anagram
         p words
       end
     end
+    matched_words = []
+    some_words.each do |new_word|
+      if new_word.split("").sort == @word.split("").sort
+        matched_words << new_word 
+      end 
+    end 
+    matched_words
   end
 
 end
