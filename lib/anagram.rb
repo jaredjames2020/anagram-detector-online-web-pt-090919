9b293f34cf9c7e72e3053db85be946fe895167d2
Jared S <jaredjames2020@gmail.com>
2019-10-13 16:32:02 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,12 +2,13 @@
 class Anagram
   attr_accessor :match
   
-  def initialize(word)
-    @word = %w(word)
+  def initialize(%w(word))
+    @word = word
   end
   
   def match
-  
+    @word.each do |words|
+      words
   
   end
 
